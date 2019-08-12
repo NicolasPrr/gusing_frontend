4dec45a0b52e46011776f82687501bfe4c2fc9c2
nicolasprr <niparrara@unal.edu.co>
2019-08-11 22:45:01 -0500

    url s

--- src/components/Urlpage.js
+++ src/components/Urlpage.js
@@ -1,4 +1,4 @@
-const baseURLFront = 'http://localhost:3001'
-// const baseURLFront = "http://192.168.1."+process.env.REACT_APP_URL + ":3001"
+// const baseURLFront = 'http://localhost:3001'
+const baseURLFront = "http://192.168.1."+process.env.REACT_APP_URL + ":3001"
 
 export default baseURLFront
