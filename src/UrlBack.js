9a8400692338e27c38bff9202fc64a973a509aac
nicolasprr <niparrara@unal.edu.co>
2019-08-16 14:11:32 -0500

    URL GUSING

--- src/UrlBack.js
+++ src/UrlBack.js
@@ -1,9 +1,9 @@
-const URLBack = 'http://192.168.0.3:3000'
+// const URLBack = 'http://192.168.0.3:3000'
 // const URLBack = 'https://gusingback.herokuapp.com'
 
 // const URLBack = 'http://localhost:3000'
 //https://gusingback.herokuapp.com//
 //http://192:168.1.118:3000
-// const URLBack = "http://192.168.1."+ process.env.REACT_APP_URL + ":3000"
+const URLBack = "http://192.168.1."+ process.env.REACT_APP_URL + ":3000"
 
 export default URLBack
